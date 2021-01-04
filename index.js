3e6faabcde56aa059c07b27870283509fc5abf0a
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2021-01-04 15:26:24 -0800

    init

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/Views/index.html"));
+app.use(express.static(__dirname + "/Views"));
 
 app.get("*", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "Views/index.html");
 });
 
 // start the server listening for requests
-app.listen(process.env.PORT || port, () => console.log("Server is running..."));
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
